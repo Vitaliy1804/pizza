0e9448137eb08885cfe2e6e0c5346d462301521f
VitliiSheremet <shervitt1982@gmail.com>
2025-04-29 22:20:46 +0300

    search with backend

--- react-pizza/src/pages/Home.jsx
+++ react-pizza/src/pages/Home.jsx
@@ -21,16 +21,17 @@ import Skeleton from '../components/PizzaBlock/Skeleton';
 
         const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
         const sortBy = sortType.sortProperty.replace('-', '');
-        const category = categoryId > 0 ? `category=${categoryId}` : ''
+        const category = categoryId > 0 ? `category=${categoryId}` : '';
+        const search = searchValue ? `&search=${searchValue}` : '';
 
-        fetch(`https://649f68bd245f077f3e9d89bf.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`)
+        fetch(`https://649f68bd245f077f3e9d89bf.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${search}`)
       .then((res)=>  res.json())
       .then((arr) => {
         setItems(arr);
         setIsLoading(false)
       })
       window.scrollTo(0, 0)
-      }, [categoryId, sortType])
+      }, [categoryId, sortType, searchValue])
 
       const pizzas = items
       .filter(obj => {
